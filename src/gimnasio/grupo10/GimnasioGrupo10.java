4cee12e3660c49139b5ddb6b2fc66279c65d1900
JesicaEstrada <101682414+JesicaEstrada@users.noreply.github.com>
2024-06-10 18:58:57 -0300

    vistas

--- src/gimnasio/grupo10/GimnasioGrupo10.java
+++ src/gimnasio/grupo10/GimnasioGrupo10.java
@@ -27,18 +27,18 @@ public class GimnasioGrupo10 {
 
     public static void main(String[] args) {
 
-        SocioData soc = new SocioData();
-        EntrenadorData ent = new EntrenadorData();
-        AsistenciaData asist = new AsistenciaData();
-        MembresiaData memb = new MembresiaData();
-        ClaseData clase = new ClaseData();
+//        SocioData soc = new SocioData();
+//        EntrenadorData ent = new EntrenadorData();
+//        AsistenciaData asist = new AsistenciaData();
+//        MembresiaData memb = new MembresiaData();
+//        ClaseData clase = new ClaseData();
         /*----------------------------------------------------------------------------------------------------------------------------*/
-        Socio socio2 = new Socio(8,"41000222", "Pedro", "Gomez", 30, "pedro.gomez@example.com", "555444", true);
-        Socio socio3 = new Socio(9,"41000333", "Maria", "Lopez", 22, "maria.lopez@example.com", "666777", false);
-        Socio socio4 = new Socio(10,"41000444", "Juan", "Perez", 28, "juan.perez@example.com", "888999", true);
-        Socio socio5 = new Socio(11,"41000555", "Ana", "Martinez", 35, "ana.martinez@example.com", "333222", false);
-        Socio socio6 = new Socio(18,"41000666", "Juan", "Romero", 25, "juan.romero@example.com", "345222", true);
-        Socio socio7 = new Socio(19,"41000113", "María", "Gómez", 35, "maria@example.com", "777888", true);
+//        Socio socio2 = new Socio(8,"41000222", "Pedro", "Gomez", 30, "pedro.gomez@example.com", "555444", true);
+//        Socio socio3 = new Socio(9,"41000333", "Maria", "Lopez", 22, "maria.lopez@example.com", "666777", false);
+//        Socio socio4 = new Socio(10,"41000444", "Juan", "Perez", 28, "juan.perez@example.com", "888999", true);
+//        Socio socio5 = new Socio(11,"41000555", "Ana", "Martinez", 35, "ana.martinez@example.com", "333222", false);
+//        Socio socio6 = new Socio(18,"41000666", "Juan", "Romero", 25, "juan.romero@example.com", "345222", true);
+//        Socio socio7 = new Socio(19,"41000113", "María", "Gómez", 35, "maria@example.com", "777888", true);
 
         //  public Socio(String dni_socio, String nombre_socio, String apellido_socio, int edad_socio, String correo_socio, String telefono_socio, boolean estado_socio) {
 //        soc.cargarSocio(socio1);
@@ -71,13 +71,13 @@ public class GimnasioGrupo10 {
 //        }
         /*----------------------------------------------------------------------------------------------------------------------------*/
 //
-        Entrenador entrenador1 = new Entrenador(8, "123456789", "John", "Smith", "Fútbol", true);
-        Entrenador entrenador2 = new Entrenador(9, "987654321", "Emily", "Johnson", "Baloncesto", true);
-        Entrenador entrenador3 = new Entrenador(10, "112233445", "Michael", "Williams", "Natación", true);
-        Entrenador entrenador4 = new Entrenador(11, "334455667", "Jessica", "Brown", "Atletismo", true);
-        Entrenador entrenador5 = new Entrenador(12, "556677889", "Christopher", "Davis", "Tenis", true);
-        Entrenador entrenador6 = new Entrenador(13, "222333222", "Juan", "González", "Tenis", true);
-        Entrenador entrenador7 = new Entrenador(14, "555444333", "María", "López", "Tenis", true);
+//        Entrenador entrenador1 = new Entrenador(8, "123456789", "John", "Smith", "Fútbol", true);
+//        Entrenador entrenador2 = new Entrenador(9, "987654321", "Emily", "Johnson", "Baloncesto", true);
+//        Entrenador entrenador3 = new Entrenador(10, "112233445", "Michael", "Williams", "Natación", true);
+//        Entrenador entrenador4 = new Entrenador(11, "334455667", "Jessica", "Brown", "Atletismo", true);
+//        Entrenador entrenador5 = new Entrenador(12, "556677889", "Christopher", "Davis", "Tenis", true);
+//        Entrenador entrenador6 = new Entrenador(13, "222333222", "Juan", "González", "Tenis", true);
+//        Entrenador entrenador7 = new Entrenador(14, "555444333", "María", "López", "Tenis", true);
 //        ent.cargarEntrenador(entrenador1);
 //        ent.cargarEntrenador(entrenador2);
 //        ent.cargarEntrenador(entrenador3);
@@ -133,16 +133,16 @@ public class GimnasioGrupo10 {
 //          }
 //         
 // Creación de las clases con especialidades específicas
-        Clase clase1 = new Clase("Entrenamiento de Tenis de Dobles", entrenador7, LocalTime.parse("08:00"), 15, true);
-        Clase clase2 = new Clase("Entrenamiento de Baloncesto de Tiro", entrenador2, LocalTime.parse("09:00"), 20, true);
-        Clase clase3 = new Clase("Clase de Natación Estilo Libre", entrenador3, LocalTime.parse("10:00"), 18, true);
-        Clase clase4 = new Clase("Entrenamiento de Atletismo de Carrera", entrenador4, LocalTime.parse("11:00"), 15, true);
-        Clase clase5 = new Clase("Entrenamiento de Tenis de Servicio", entrenador5, LocalTime.parse("12:00"), 20, true);
-        Clase clase6 = new Clase("Entrenamiento de Fútbol de Pases", entrenador1, LocalTime.parse("13:00"), 18, true);
-        Clase clase7 = new Clase("Entrenamiento de Tenis de Golpes de Fondo", entrenador7, LocalTime.parse("14:00"), 15, true);
-        Clase clase8 = new Clase("Entrenamiento de Tenis de Volea", entrenador6, LocalTime.parse("15:00"), 15, true);
-        Clase clase9 = new Clase("Entrenamiento de Baloncesto de Defensa", entrenador2, LocalTime.parse("16:00"), 20, true);
-        Clase clase10 = new Clase("Entrenamiento de Fútbol de Remates", entrenador1, LocalTime.parse("17:00"), 18, true);
+//        Clase clase1 = new Clase("Entrenamiento de Tenis de Dobles", entrenador7, LocalTime.parse("08:00"), 15, true);
+//        Clase clase2 = new Clase("Entrenamiento de Baloncesto de Tiro", entrenador2, LocalTime.parse("09:00"), 20, true);
+//        Clase clase3 = new Clase("Clase de Natación Estilo Libre", entrenador3, LocalTime.parse("10:00"), 18, true);
+//        Clase clase4 = new Clase("Entrenamiento de Atletismo de Carrera", entrenador4, LocalTime.parse("11:00"), 15, true);
+//        Clase clase5 = new Clase("Entrenamiento de Tenis de Servicio", entrenador5, LocalTime.parse("12:00"), 20, true);
+//        Clase clase6 = new Clase("Entrenamiento de Fútbol de Pases", entrenador1, LocalTime.parse("13:00"), 18, true);
+//        Clase clase7 = new Clase("Entrenamiento de Tenis de Golpes de Fondo", entrenador7, LocalTime.parse("14:00"), 15, true);
+//        Clase clase8 = new Clase("Entrenamiento de Tenis de Volea", entrenador6, LocalTime.parse("15:00"), 15, true);
+//        Clase clase9 = new Clase("Entrenamiento de Baloncesto de Defensa", entrenador2, LocalTime.parse("16:00"), 20, true);
+//        Clase clase10 = new Clase("Entrenamiento de Fútbol de Remates", entrenador1, LocalTime.parse("17:00"), 18, true);
 
 //        clase.cargarClase(clase1);
 //        clase.cargarClase(clase2);
@@ -180,21 +180,21 @@ public class GimnasioGrupo10 {
 //        if (clasehorario != null) {
 //            System.out.println(clasehorario.toString());
 //        }
-        Membresia membresia1 = new Membresia(socio2, 10, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 12, 1), 200.0, true);
-        Membresia membresia2 = new Membresia(socio3, 5, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 9, 1), 100.0, false);
-        Membresia membresia3 = new Membresia(socio4, 15, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 12, 1), 300.0, true);
-        Membresia membresia4 = new Membresia(socio5, 20, LocalDate.of(2024, 6, 1), LocalDate.of(2025, 6, 1), 400.0, false);
-        Membresia membresia5 = new Membresia(socio6, 8, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 8, 1), 160.0, true);
-        Membresia membresia6 = new Membresia(socio7, 15, LocalDate.of(2023, 6, 1), LocalDate.of(2024, 1, 1), 240.0, false);
-        Membresia membresia7 = new Membresia(socio7, 12, LocalDate.of(2024, 2, 1), LocalDate.of(2024, 6, 6), 240.0, true);
+//        Membresia membresia1 = new Membresia(socio2, 10, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 12, 1), 200.0, true);
+//        Membresia membresia2 = new Membresia(socio3, 5, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 9, 1), 100.0, false);
+//        Membresia membresia3 = new Membresia(socio4, 15, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 12, 1), 300.0, true);
+//        Membresia membresia4 = new Membresia(socio5, 20, LocalDate.of(2024, 6, 1), LocalDate.of(2025, 6, 1), 400.0, false);
+//        Membresia membresia5 = new Membresia(socio6, 8, LocalDate.of(2024, 6, 1), LocalDate.of(2024, 8, 1), 160.0, true);
+//        Membresia membresia6 = new Membresia(socio7, 15, LocalDate.of(2023, 6, 1), LocalDate.of(2024, 1, 1), 240.0, false);
+//        Membresia membresia7 = new Membresia(socio7, 12, LocalDate.of(2024, 2, 1), LocalDate.of(2024, 6, 6), 240.0, true);
 
-        memb.cargarMembresia(membresia1);
-        memb.cargarMembresia(membresia2);
-        memb.cargarMembresia(membresia3);
-        memb.cargarMembresia(membresia4);
-        memb.cargarMembresia(membresia5);
-        memb.cargarMembresia(membresia6);
-        memb.cargarMembresia(membresia7);
+//        memb.cargarMembresia(membresia1);
+//        memb.cargarMembresia(membresia2);
+//        memb.cargarMembresia(membresia3);
+//        memb.cargarMembresia(membresia4);
+//        memb.cargarMembresia(membresia5);
+//        memb.cargarMembresia(membresia6);
+//        memb.cargarMembresia(membresia7);
 //        List<Membresia> memSocio = memb.membresiaPorSocio(socio7.getId_socio());
 //        if (!memSocio.isEmpty()) {
 //            for (Membresia membresia : memSocio) {
@@ -211,6 +211,7 @@ public class GimnasioGrupo10 {
 //Socio socio3 = new Socio(9, "41000333", "Maria", "Lopez", 22, "maria.lopez@example.com", "666777", false);
 //        Asistencia asistencia2 = new Asistencia(socio3, clase2, LocalDate.of(2024, 10, 6));
 //        asist.guardarAsistencia(asistencia2);
+            
         }
 
     }
